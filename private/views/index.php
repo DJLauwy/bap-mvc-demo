43c3ac90015eb994998c5b065de13d6c5a723e22
Lauwy de Hoop <55271928+DJLauwy@users.noreply.github.com>
2020-06-11 12:46:27 +0200

    beginnetje
    op het moment bezig met de verificatiemail

---
+++ private/views/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>verificatiemail bap</title>
+	</head>
+	<body>
+		<h1>account aanmaken</h1>
+		<form action="register.php" method="post">
+			<input type="text" placeholder="naam" name="naam" required>
+			<input type="email" placeholder="e-mail adres" name="email" required>
+			<input type="password" placeholder="wachtwoord" name="wachtwoord" required>
+			<input type="submit" value="registreren">
+		</form>
+	</body>
+</html>
